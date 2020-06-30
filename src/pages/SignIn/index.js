882747ef856d83302c74e2ad5eb7afa1fbb3067c
Molinux <cybercrowbr@gmail.com>
2020-06-29 23:30:14 -0300

    First components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SingIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
