47869964b7688de8540730943afc1ce90edfd01e
Molinux <cybercrowbr@gmail.com>
2020-07-04 16:26:58 -0300

    Appointments from API

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ onCancel, data }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
           source={{
-            uri:
-              'https://avatars.dicebear.com/api/bottts/leonidas.svg?w[]=32px',
+            uri: `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
+            // uri: data.provider.avatar
+            //   ? data.provider.avatar.url
+            //   : 'https://api.adorable.io/avatar/50/rocketseat.png',
+            // 'https://avatars.dicebear.com/api/bottts/leonidas.svg?w[]=32px',
+            // 'https://avatars.dicebear.com/api/bottts/leonidas.svg',
           }}
         />
 
         <Info>
-          <Name>Marcus Molinero</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {/* {data.cancelable && !data.canceled_at && ( */}
+      {/* {data.cancelable && ( */}
+      {!data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
